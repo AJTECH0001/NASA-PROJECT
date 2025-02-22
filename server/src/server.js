219d709144b48d31c0babebca344304205244462
AJTECH <145960311+AJTECH0001@users.noreply.github.com>
2025-02-22 23:51:52 +0100

    project updated

--- server/src/server.js
+++ server/src/server.js
@@ -2,12 +2,19 @@ const http = require('http');
 
 const app = require('./app');
 
-const PORT = Process.env.PORT || 8000;
+const {loadPlanetData} = require('./models/planets.model');
+
+const PORT =  8000;
 
 const server = http.createServer(app);
+
+async function startServer() {
+    await loadPlanetData();
+}
 
 
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
 });
 
+startServer();
